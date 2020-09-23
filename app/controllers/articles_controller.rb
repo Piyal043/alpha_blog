096e1769c590183f1d334fdb0392b575ebfcf793
Peyal <mohammad.piyal@gmail.com>
2020-09-23 16:55:15 +0600

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :updata, :destroy]
 
   def index
     @articles = Article.paginate(page: params[:page], per_page:5)
@@ -15,7 +17,7 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     # @article.save
     # redirect_to articles_path(@article)
     #actuall
@@ -55,6 +57,13 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit and delete your own articles"
+      redirect_to root_path
+    end
   end
 
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+//= require jquery
+//= require jquery_ujs
+
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require_tree .
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
